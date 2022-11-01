6e4dd0edc4102ef2252d9bd346b2c16a2f76f162
ImmanuelArvellian <arvellianputra@gmail.com>
2022-11-01 15:36:02 +0700

    Proximity fix

--- app/src/main/java/com/example/gd9_proximity_a_10905/MainActivity.kt
+++ app/src/main/java/com/example/gd9_proximity_a_10905/MainActivity.kt
@@ -12,32 +12,17 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    // on below line we are creating
+    // a variable for our text view.
     lateinit var sensorStatusTV: TextView
+
+    // on below line we are creating
+    // a variable for our proximity sensor
     lateinit var proximitySensor: Sensor
+
+    // on below line we are creating
+    // a variable for sensor manager
     lateinit var sensorManager: SensorManager
-
-    var proximitySensorEventListener: SensorEventListener? = object: SensorEventListener{
-        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onSensorChanged(event: SensorEvent?) {
-            if (event != null) {
-                if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
-                    if (event.values[0] == 0f) {
-                        // here we are setting our status to our textview..
-                        // if sensor event return 0 then object is closed
-                        // to sensor else object is away from sensor.
-                        sensorStatusTV.text = "<<<Near>>>"
-                    } else {
-                        // on below line we are setting text for text view
-                        // as object is away from sensor.
-                        sensorStatusTV.text = "<<<<Away>>>>"
-                    }
-                }
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,12 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         // on below line we are initializing our all variables.
         sensorStatusTV = findViewById(R.id.idTVSensorStatus)
+
         // on below line we are initializing our sensor manager
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
         // on below line we are initializing our proximity sensor variable
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
-        // on below line we are checking if the proximity sensor is null
 
+        // on below line we are checking if the proximity sensor is null
         if (proximitySensor == null) {
             // on below line we are displaying a toast if no sensor is available
             Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
@@ -63,6 +50,30 @@ class MainActivity : AppCompatActivity() {
                 proximitySensor,
                 SensorManager.SENSOR_DELAY_NORMAL
             )
+        }
+    }
+
+    // calling the sensor event class to detect
+    // the change in data when sensor starts working.
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+            // method to check accuracy changed in sensor.
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            // check if the sensor type is proximity sensor.
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0f) {
+                    // here we are setting our status to our textview..
+                    // if sensor event return 0 then object is closed
+                    // to sensor else object is away from sensor.
+                    sensorStatusTV.text = "<<<Near>>>"
+                } else {
+                    // on below line we are setting text for text view
+                    // as object is away from sensor.
+                    sensorStatusTV.text = "<<<<Away>>>>"
+                }
+            }
         }
     }
 }
